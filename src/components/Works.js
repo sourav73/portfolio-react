f23f7553da6d371a474e1f7e4c12a143c406043e
sourav73 <mdsehab1@gmail.com>
2022-06-02 00:26:40 +0600

    unused vars from works.js removed

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import WorkItem from './WorkItem';
-import Isotope from 'isotope-layout';
-import portfolioWorks from '../data/portfolioWorks.json';
+import WorkItem from "./WorkItem";
+import Isotope from "isotope-layout";
+import portfolioWorks from "../data/portfolioWorks.json";
 
 const Works = () => {
-  const [isChecked, setIsChecked] = useState(false);
   // init one ref to store the future isotope object
   const isotope = React.useRef();
   // store the filter keyword in a state
-  const [filterKey, setFilterKey] = React.useState('*')
+  const [filterKey, setFilterKey] = React.useState("*");
 
   // initialize an Isotope object with configs
   React.useEffect(() => {
-    isotope.current = new Isotope('.isotope-container', {
-      itemSelector: '.work-item',
+    isotope.current = new Isotope(".isotope-container", {
+      itemSelector: ".work-item",
       // layoutMode: 'fitRows',
       masonry: {
         fitWidth: true,
-        gutter: 16
-      }
-    })
+        gutter: 16,
+      },
+    });
     // cleanup
     return () => isotope.current.destroy();
-  }, [])
+  }, []);
 
   // handling filter key change
   React.useEffect(() => {
-    filterKey === '*'
+    filterKey === "*"
       ? isotope.current.arrange({ filter: `*` })
-      : isotope.current.arrange({ filter: `.${filterKey}` })
+      : isotope.current.arrange({ filter: `.${filterKey}` });
   }, [filterKey]);
 
   const handleFilterKeyChange = (key) => () => {
@@ -37,48 +36,68 @@ const Works = () => {
   };
 
   const handleFilterButtons = (e) => {
-    if (e.target.nodeName === 'SPAN' || e.target.nodeName === 'BUTTON') {
-      document.querySelector('.filters-button-group .button.is-checked').classList.remove('is-checked');
-      if (e.target.nodeName === 'SPAN') {
-        e.target.parentElement.classList.add('is-checked');
+    if (e.target.nodeName === "SPAN" || e.target.nodeName === "BUTTON") {
+      document
+        .querySelector(".filters-button-group .button.is-checked")
+        .classList.remove("is-checked");
+      if (e.target.nodeName === "SPAN") {
+        e.target.parentElement.classList.add("is-checked");
       }
-      e.target.classList.add('is-checked');
+      e.target.classList.add("is-checked");
     }
-  }
+  };
 
   return (
     <section id="works" className="my-5 pb-5">
       <div className="container">
         <div className="vertical-heading">
           <h3>Find My Work</h3>
-          <h2 className="">My <br />Amazing <span>Work</span></h2>
+          <h2 className="">
+            My <br />
+            Amazing <span>Work</span>
+          </h2>
         </div>
       </div>
       <div className="my-works">
         <div className="filters-button-group" onClick={handleFilterButtons}>
-          <button className="button is-checked" onClick={handleFilterKeyChange('*')}><span>All</span></button>
-          <button className="button" onClick={handleFilterKeyChange('sass')}><span>Sass</span></button>
-          <button className="button" onClick={handleFilterKeyChange('bootstrap')}><span>Bootstrap</span></button>
-          <button className="button" onClick={handleFilterKeyChange('react')}><span>React-JS</span></button>
+          <button
+            className="button is-checked"
+            onClick={handleFilterKeyChange("*")}
+          >
+            <span>All</span>
+          </button>
+          <button className="button" onClick={handleFilterKeyChange("sass")}>
+            <span>Sass</span>
+          </button>
+          <button
+            className="button"
+            onClick={handleFilterKeyChange("bootstrap")}
+          >
+            <span>Bootstrap</span>
+          </button>
+          <button className="button" onClick={handleFilterKeyChange("react")}>
+            <span>React-JS</span>
+          </button>
         </div>
 
         <div className="isotope-container">
-
           {portfolioWorks.map((workItem) => {
-            return <WorkItem
-              key={workItem.id}
-              allCategory={workItem.allCategory}
-              imageSrc={workItem.imageSrc}
-              imageAlt={workItem.imageAlt}
-              usedTechnology={workItem.usedTechnology}
-              sourceUrl={workItem.sourceUrl}
-              demoUrl={workItem.demoUrl} />
+            return (
+              <WorkItem
+                key={workItem.id}
+                allCategory={workItem.allCategory}
+                imageSrc={workItem.imageSrc}
+                imageAlt={workItem.imageAlt}
+                usedTechnology={workItem.usedTechnology}
+                sourceUrl={workItem.sourceUrl}
+                demoUrl={workItem.demoUrl}
+              />
+            );
           })}
         </div>
       </div>
-
     </section>
   );
-}
+};
 
 export default Works;
